1a034b2f3f6e8459360e56799845fcaf9baec907
viren sureja <virensureja3@gmail.com>
2021-02-25 22:03:36 +0530

    added custom state-hook

--- basics/react/dojo-blog/src/Home.js
+++ basics/react/dojo-blog/src/Home.js
@@ -1,31 +1,17 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-	const [blogs, setBlogs] = useState([
-		{ title: 'My new blog', body: 'lorem ipsum...', author: 'viren', id: 1 },
-		{ title: 'Welcome user', body: 'lorem ipsum...', author: 'mario', id: 2 },
-		{ title: 'support me!', body: 'lorem ipsum...', author: 'krish', id: 3 },
-		{ title: 'i"m lucky', body: 'lorem ipsum...', author: 'mario', id: 4 },
-	]);
-	const [name, setName] = useState('mario');
-
-	const handleDelete = (id) => {
-		const newBlogs = blogs.filter((blog) => blog.id !== id);
-		setBlogs(newBlogs);
-	};
-
-	useEffect(() => {
-		console.log('useEffect is ran!');
-	}, []);
+	const { data: blogs, isPending, error } = useFetch(
+		'http://localhost:8000/blogs'
+	);
 
 	return (
 		<div className="home">
-			<BlogList
-				blogs={blogs}
-				title="All Blogs"
-				handleDelete={handleDelete}
-			/>
+			{error && <div>{error}</div>}
+			{isPending && <div>Loading...</div>}
+			{blogs && <BlogList blogs={blogs} title="All Blogs" />}
 		</div>
 	);
 };
